7241cb71d34f93b37592c41761139930ffb00329
Hieubtt <trunghieu17062001@gmail.com>
2025-12-01 19:00:11 +0700

    TESTLAN1

--- buoi 5/bt1.py
+++ buoi 5/bt1.py
@@ -2,7 +2,7 @@ n = int(input('Nhập số lượng n : '))
 List_number = []
 
 for i in range(n):
-    number_1 = int(input(f'Nhập số lượng {n} phần tử'))
+    number_1 = int(input(f'Nhập số lượng {i+1} phần tử : '))
     List_number.append(number_1)
 print(List_number) 
 
