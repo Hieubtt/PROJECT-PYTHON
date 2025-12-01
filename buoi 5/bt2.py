a3a3deb3a4670dc6aec22f6adbbc900358654e5d
Hieubtt <trunghieu17062001@gmail.com>
2025-12-01 19:01:17 +0700

    TESTLAN2

---
+++ buoi 5/bt2.py
@@ -0,0 +1,6 @@
+chuoi_ki_tu = input('Nhập chuỗi kí tự : ')
+
+
+#List_CKT = [chuoi_ki_tu]
+
+print('Số ký tự trong chuỗi là : ', len(chuoi_ki_tu))
---
+++ buoi 5/bt3.py
@@ -0,0 +1,4 @@
+chuoi_ki_tu  =  input('Nhập chuỗi kí tự : ')
+
+result_chuoi_ki_tu = chuoi_ki_tu.upper()
+print(result_chuoi_ki_tu)
