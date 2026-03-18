9993f4dc98648baa9f1c3fa8ddef9e96218010ac
Hieubtt <trunghieu17062001@gmail.com>
2026-03-18 18:06:44 +0700

    20260318_01

--- FINAL_DEMO/dags/etl_dags.py
+++ FINAL_DEMO/dags/etl_dags.py
@@ -11,7 +11,7 @@ from airflow.utils.dates import days_ago
 DAG_ID = "saleFinancial_risk_assessment_to_dw"
 
 # INGEST_SCRIPT = "/opt/airflow/dags/scripts/load_csv_to_postgres_raw.py"
-# ETL_SCRIPT    = "/opt/airflow/dags/scripts/etl_sales_transform_load.py"
+ETL_SCRIPT    = "/opt/airflow/scripts/extract_fin_risk_csv.py"
 # CHECK_SCRIPT  = "/opt/airflow/dags/scripts/post_run_check.py"
 
 PYTHON = "python"
@@ -19,43 +19,30 @@ PYTHON = "python"
 default_args = {"owner": "hieubtt", "retries": 2, "retry_delay": timedelta(minutes=1)}
 
 DB_ENV = {
-    "DB_HOST": os.getenv("DB_HOST", "postgres"),
+    "DB_HOST": os.getenv("DB_HOST", "fin_risk_assessment_db"), #postgres_airflow mai check lai cho nay
     "DB_PORT": os.getenv("DB_PORT", "5432"),
-    "DB_NAME": os.getenv("DB_NAME", "fin_risk_assessment_db"),
-    "DB_USER": os.getenv("DB_USER", "admin"),#airflow
-    "DB_PASS": os.getenv("DB_PASS", "admin"),#airflow
-    "CSV_PATH": os.getenv("CSV_PATH", "/opt/airflow/dags/data/sales_raw.csv"),
+    "DB_NAME": os.getenv("DB_NAME", "etl_fin_db"),
+    "DB_USER": os.getenv("DB_USER", "admin"),
+    "DB_PASS": os.getenv("DB_PASS", "admin")
 }
 
 with DAG(
     dag_id=DAG_ID,
-    description="E2E demo: CSV -> Postgres(raw) -> DW using ETL + Airflow",
+    description="CSV -> Postgres ETL",
     default_args=default_args,
     start_date=days_ago(1),
     schedule_interval=None,
     catchup=False,
-    tags=["demo", "sales", "etl"],
 ) as dag:
+
     start = EmptyOperator(task_id="start")
 
-    ingest = BashOperator(
-        task_id="ingest_csv_to_raw",
-        bash_command=f'',
-        env=DB_ENV,
-    )
-
-    transform_load = BashOperator(
-        task_id="transform_load_dw",
-        bash_command=f'',
-        env=DB_ENV,
-    )
-
-    check = BashOperator(
-        task_id="post_run_check",
-        bash_command=f'',
+    etl = BashOperator(
+        task_id="run_etl",
+        bash_command=f'{PYTHON} {ETL_SCRIPT}',
         env=DB_ENV,
     )
 
     end = EmptyOperator(task_id="end")
 
-    start >> ingest >> transform_load >> check >> end
+    start >> etl >> end
--- FINAL_DEMO/scripts/extract_fin_risk_csv.py
+++ FINAL_DEMO/scripts/extract_fin_risk_csv.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 import os
 import sys
 import uuid
-
+import pandas as pd
 import psycopg2
 
 RUN_ID = f"RUN_{uuid.uuid4().hex[:8]}"
+
 
 def env(name: str, default: str | None = None) -> str:
     v = os.getenv(name)
@@ -17,130 +18,104 @@ def env(name: str, default: str | None = None) -> str:
     return v.strip()
 
 DB = {
-    "host": env("DB_HOST", "postgres"),
-    "port": int(env("DB_PORT", "5435")),
-    "database": env("DB_NAME", "fin_risk_assessment_db"),
-    "user": env("DB_USER", "admin"),
-    "password": env("DB_PASS", "admin"),
-    "connect_timeout": int(env("DB_TIMEOUT", "10")),
+    "host": os.getenv("DB_HOST", "fin_risk_assessment_db"),
+    "port": os.getenv("DB_PORT", "5435"),
+    "database": os.getenv("DB_NAME", "fin_etl_db"),
+    "user": os.getenv("DB_USER", "admin"),
+    "password": os.getenv("DB_PASS", "admin"),
 }
 
-SQL_CREATE_TABLE_LOG = '''
-  CREATE TABLE ETL_LOG 
-  (
-    run_id INT PRIMARY KEY, 
-    step_name TEXT, 
-    status TEXT, 
-    message TEXT
-  );
+SQL_CREATE_DATABASE = '''
+  CREATE DATABASE IF NOT EXISTS fin_etl_db;
   '''
-# CREATE TABLE fin_risk_assessment (
-# Age INT,
-# Gender TEXT,
-# Education_Level TEXT,
-# Marital_Status TEXT,
-# Income INT NULL,
-# Credit_Score INT NULL,
-# Loan_Amount FLOAT NULL,
-# Loan_Purpose  NULL,
-# Employment_Status TEXT NULL,
-# Years_at_Current_Job INT,
-# Payment_History TEXT NULL,
-# Debt_to_Income_Ratio FLOAT NULL,
-# Assets_Value TEXT,
-# Number_of_Dependents INT NULL,
-# City TEXT NULL, 
-# State VARCHAR(2) NULL,
-# Country TEXT NULL,
-# Previous_Defaults TEXT NULL, 
-# Marital_Status_Change TEXT NULL, 
-# Risk_Rating TEXT NULL
-# )
+SQL_CREATE_TABLE_JOB = '''
+
+CREATE TABLE IF NOT EXISTS etl_log(run_id TEXT, step_name TEXT, status TEXT, message TEXT, message TEXT) ;
+'''
+
+
 
 SQL_LOG = "INSERT INTO etl_log(run_id, step_name, status, message) VALUES (%s,%s,%s,%s);"
 
 SQL_BUILD_STAGING = r'''
-DROP TABLE IF EXISTS etl.sales_staging_clean;
+DROP TABLE IF EXISTS fin_risk_assessment;
 
-CREATE TABLE etl.sales_staging_clean (
-  sale_id        TEXT PRIMARY KEY,
-  sale_date      DATE NOT NULL,
-  customer_id    TEXT NOT NULL,
-  product_id     TEXT NOT NULL,
-  quantity       INT  NOT NULL CHECK (quantity > 0),
-  unit_price     NUMERIC(10,2) NOT NULL CHECK (unit_price >= 0),
-  payment_method TEXT NOT NULL CHECK (payment_method IN ('cash','card','ewallet')),
-  store_city     TEXT
+CREATE TABLE fin_risk_assessment (
+Age INT,
+Gender VARCHAR(50),
+Education_Level VARCHAR(50),
+Marital_Status VARCHAR(50),
+Income FLOAT NULL,
+Credit_Score FLOAT NULL,
+Loan_Amount FLOAT NULL,
+Loan_Purpose  VARCHAR(50) NULL,
+Employment_Status VARCHAR(50) NULL,
+Years_at_Current_Job FLOAT NULL,
+Payment_History VARCHAR(50) NULL,
+Debt_to_Income_Ratio FLOAT NULL,
+Assets_Value FLOAT NULL,
+Number_of_Dependents FLOAT NULL,
+City VARCHAR(250) NULL, 
+State VARCHAR(2) NULL,
+Country VARCHAR(250) NULL,
+Previous_Defaults FLOAT NULL, 
+Marital_Status_Change FLOAT NULL, 
+Risk_Rating VARCHAR(50) NULL
 );
-
-WITH parsed AS (
-  SELECT
-    raw_id,
-    NULLIF(sale_id_text,'') AS sale_id,
-    CASE
-      WHEN sale_date_text ~ '^\d{4}-\d{2}-\d{2}$' THEN sale_date_text::DATE
-      ELSE NULL
-    END AS sale_date,
-    NULLIF(customer_id_text,'') AS customer_id,
-    NULLIF(product_id_text,'')  AS product_id,
-    CASE
-      WHEN quantity_text ~ '^[-+]?[0-9]+$' THEN quantity_text::INT
-      ELSE NULL
-    END AS quantity,
-    CASE
-      WHEN unit_price_text ~ '^[-+]?[0-9]*\.?[0-9]+$' THEN unit_price_text::NUMERIC(10,2)
-      ELSE NULL
-    END AS unit_price,
-    payment_method,
-    store_city
-  FROM raw.sales_raw
-),
-dedup AS (
-  SELECT DISTINCT ON (sale_id)
-    sale_id, sale_date, customer_id, product_id, quantity, unit_price, payment_method, store_city, raw_id
-  FROM parsed
-  WHERE sale_id IS NOT NULL
-  ORDER BY sale_id, raw_id DESC
-),
-cleaned AS (
-  SELECT sale_id, sale_date, customer_id, product_id, quantity, unit_price, payment_method, store_city
-  FROM dedup
-  WHERE sale_date IS NOT NULL
-    AND customer_id IS NOT NULL
-    AND product_id IS NOT NULL
-    AND quantity IS NOT NULL AND quantity > 0
-    AND unit_price IS NOT NULL AND unit_price >= 0
-    AND payment_method IN ('cash','card','ewallet')
-)
-INSERT INTO etl.sales_staging_clean
-  (sale_id, sale_date, customer_id, product_id, quantity, unit_price, payment_method, store_city)
-SELECT sale_id, sale_date, customer_id, product_id, quantity, unit_price, payment_method, store_city
-FROM cleaned;
 '''
-
-SQL_UPSERT_DW = '''
-INSERT INTO dw.sales_fact(
-  sale_id, sale_date, customer_id, product_id, quantity, unit_price, revenue, payment_method, store_city
+# pf = pd.read_csv(r'C:\Users\TrungHieu\Documents\GitHub\PROJECT-PYTHON\FINAL_DEMO\data\Financial_risk_assessment.csv')
+pf = pd.read_csv('/opt/airflow/data/Financial_risk_assessment.csv')
+SQL_BUILD = """
+INSERT INTO fin_risk_assessment (
+    Age, Gender, Education_Level, Marital_Status, Income, Credit_Score,
+    Loan_Amount, Loan_Purpose, Employment_Status, Years_at_Current_Job,
+    Payment_History, Debt_to_Income_Ratio, Assets_Value,
+    Number_of_Dependents, City, State, Country,
+    Previous_Defaults, Marital_Status_Change, Risk_Rating
 )
-SELECT
-  sale_id, sale_date, customer_id, product_id, quantity, unit_price,
-  (quantity * unit_price)::NUMERIC(12,2) AS revenue,
-  payment_method, store_city
-FROM etl.sales_staging_clean
-ON CONFLICT (sale_id) DO UPDATE
-SET sale_date      = EXCLUDED.sale_date,
-    customer_id    = EXCLUDED.customer_id,
-    product_id     = EXCLUDED.product_id,
-    quantity       = EXCLUDED.quantity,
-    unit_price     = EXCLUDED.unit_price,
-    revenue        = EXCLUDED.revenue,
-    payment_method = EXCLUDED.payment_method,
-    store_city     = EXCLUDED.store_city,
-    loaded_at      = NOW();
-'''
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+"""
+# conn = psycopg2.connect(**DB)
+# conn.autocommit = False
+# cur = conn.cursor()
+i = 0
+a=0
+while i < len(pf):
+  row = pf.iloc[i]
+  values = (
+  float(row['Age']) if row['Age'] is not None else None,
+  str(row['Gender']) if row['Gender'] is not None else None,
+  str(row['Education Level']) if row['Education Level'] is not None else None,
+  str(row['Marital Status']) if row['Marital Status'] is not None else None,
+  float(row['Income']) if row['Income'] is not None else None,
+  float(row['Credit Score']) if row['Credit Score'] is not None else None,
+  float(row['Loan Amount']) if row['Loan Amount'] is not None else None,
+  str(row['Loan Purpose']) if row['Loan Purpose'] is not None else None,
+  str(row['Employment Status']) if row['Employment Status'] is not None else None,
+  float(row['Years at Current Job']) if row['Years at Current Job'] is not None else None,
+  str(row['Payment History']) if row['Payment History'] is not None else None,
+  float(row['Debt-to-Income Ratio']) if row['Debt-to-Income Ratio'] is not None else None,
+  float(row['Assets Value']) if row['Assets Value'] is not None else None,
+  float(row['Number of Dependents']) if row['Number of Dependents'] is not None else None,
+  str(row['City']) if row['City'] is not None else None,
+  str(row['State']) if row['State'] is not None else None,
+  str(row['Country']) if row['Country'] is not None else None,
+  float(row['Previous Defaults']) if row['Previous Defaults'] is not None else None,
+  float(row['Marital Status Change']) if row['Marital Status Change'] is not None else None,
+  str(row['Risk Rating']) if row['Risk Rating'] is not None else None
+)
+  #print(values)
+  if i % 1000 == 0: 
+    a += 1
+    print(f'Đã chạy được lần {a} 1000 dòng dữ liệu và hiện đang tiếp tục ...')
+  # if i == len(pf): 
+  #   print('Đã hoàn thành việc insert dữ liệu .')  
+  # cur.execute(SQL_BUILD, values)
 
-SQL_VALIDATE_DUP = "SELECT sale_id, COUNT(*) FROM dw.sales_fact GROUP BY sale_id HAVING COUNT(*)>1;"
-SQL_COUNT = "SELECT (SELECT COUNT(*) FROM etl.sales_staging_clean), (SELECT COUNT(*) FROM dw.sales_fact);"
+  i += 1
+
+#SQL_VALIDATE_DUP = "SELECT sale_id, COUNT(*) FROM fin_risk_assessment" # chưa can su dung nen de vay
+#SQL_COUNT = "SELECT (SELECT COUNT(*) FROM etl.sales_staging_clean), (SELECT COUNT(*) FROM dw.sales_fact);"
 
 def log(cur, step: str, status: str, msg: str = ""):
     cur.execute(SQL_LOG, (RUN_ID, step, status, msg))
@@ -150,27 +125,37 @@ def main():
     conn.autocommit = False
     try:
         with conn.cursor() as cur:
+            cur.execute(SQL_CREATE_DATABASE)
+            cur.execute(SQL_CREATE_TABLE_JOB)
             log(cur,"PIPELINE","START","Transform + Load sales")
             log(cur,"TRANSFORM","START","Build staging")
             cur.execute(SQL_BUILD_STAGING)
+            
+            data = []
+
+            data = [tuple(row) for _, row in pf.iterrows()] # dung cho ten cot csv trung voi ten cot db
+            cur.executemany(SQL_BUILD, data)  # insert tung dòng data 
+            # cur.execute(SQL_CREATE_TABLE_LOG)
+            # log(cur,"TRANSFORM","SUCCESS","Staging built")
+            
             log(cur,"TRANSFORM","SUCCESS","Staging built")
 
-            log(cur,"LOAD","START","Upsert to DW")
-            cur.execute(SQL_UPSERT_DW)
-            log(cur,"LOAD","SUCCESS","Upsert done")
+            # log(cur,"LOAD","START","Upsert to DW")
+            # cur.execute(SQL_UPSERT_DW)
+            # log(cur,"LOAD","SUCCESS","Upsert done")
 
-            log(cur,"VALIDATE","START","Check duplicates + counts")
-            cur.execute(SQL_VALIDATE_DUP)
-            if cur.fetchall():
-                raise RuntimeError("Duplicate sale_id detected in dw.sales_fact")
+            # log(cur,"VALIDATE","START","Check duplicates + counts")
+            # cur.execute(SQL_VALIDATE_DUP)
+            # if cur.fetchall():
+            #     raise RuntimeError("Duplicate sale_id detected in dw.postgres")
 
-            cur.execute(SQL_COUNT)
-            staging_cnt, dw_cnt = cur.fetchone()
-            if staging_cnt <= 0:
-                raise RuntimeError("staging_cnt=0 (no valid rows after cleaning)")
-            log(cur,"VALIDATE","SUCCESS",f"staging_cnt={staging_cnt}, dw_cnt={dw_cnt}")
+            # # cur.execute(SQL_COUNT)
+            # staging_cnt, dw_cnt = cur.fetchone()
+            # if staging_cnt <= 0:
+            #     raise RuntimeError("staging_cnt=0 (no valid rows after cleaning)")
+            # log(cur,"VALIDATE","SUCCESS",f"staging_cnt={staging_cnt}, dw_cnt={dw_cnt}")
 
-            log(cur,"PIPELINE","SUCCESS","ETL finished")
+            # log(cur,"PIPELINE","SUCCESS","ETL finished")
         conn.commit()
         print(f"ETL PASSED | RUN_ID={RUN_ID}")
         sys.exit(0)
